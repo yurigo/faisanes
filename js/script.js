94175004b9342709c429e8d7a88e71e72b7c1c8d
Yuri <ygonzoo@gmail.com>
2022-07-07 11:22:34 +0200

    🐦Hello pheasant! 🐦

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+var myAudio = document.getElementById('moonlight');
+var mute = document.getElementById('mute');
+var play = document.getElementById('play');
+
+play.style.display = 'none';
+mute.style.display = 'none';
+
+if (myAudio.duration > 0 && !myAudio.paused) {
+    //Its playing...do your job
+    console.log("it's playing")
+    mute.style.display = 'block';
+
+} else {
+    //Not playing...maybe paused, stopped or never played.
+    console.log("Ooohhh :(")
+    play.style.display = 'block';
+}
+
+
+play.onclick = () => {
+    myAudio.play();
+    play.style.display = 'none';
+    mute.style.display = 'block';
+}
+
+mute.onclick = () => {
+    myAudio.pause();
+    mute.style.display = 'none';
+    play.style.display = 'block';
+}
