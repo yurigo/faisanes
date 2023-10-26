3472166fac4125b53504d9154d53af493e6f4af7
Yuri <ygonzoo@gmail.com>
2023-10-26 10:17:20 +0200

    change button and add title
    - prettify

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
+var myAudio = document.getElementById("moonlight");
+var mute = document.getElementById("mute");
+var play = document.getElementById("play");
 
-var myAudio = document.getElementById('moonlight');
-var mute = document.getElementById('mute');
-var play = document.getElementById('play');
+play.style.display = "none";
+mute.style.display = "none";
 
-play.style.display = 'none';
-mute.style.display = 'none';
+if (myAudio.paused) {
+  myAudio.play();
+  myAudio.pause();
+}
 
 if (myAudio.duration > 0 && !myAudio.paused) {
-    //Its playing...do your job
-    console.log("it's playing")
-    mute.style.display = 'block';
-
+  //Its playing...do your job
+  console.log("it's playing");
+  mute.style.display = "block";
 } else {
-    //Not playing...maybe paused, stopped or never played.
-    console.log("Ooohhh :(")
-    play.style.display = 'block';
+  //Not playing...maybe paused, stopped or never played.
+  console.log("Ooohhh :(");
+  play.style.display = "block";
 }
-
 
 play.onclick = () => {
-    myAudio.play();
-    play.style.display = 'none';
-    mute.style.display = 'block';
-}
+  myAudio.play();
+  play.style.display = "none";
+  mute.style.display = "block";
+};
 
 mute.onclick = () => {
-    myAudio.pause();
-    mute.style.display = 'none';
-    play.style.display = 'block';
-}
+  myAudio.pause();
+  mute.style.display = "none";
+  play.style.display = "block";
+};
